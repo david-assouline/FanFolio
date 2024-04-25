c0c5a86ae241b6529bd35639c98c02317b3f5404
David <david.assouline@hotmail.com>
2024-04-24 23:57:30 -0400

    work on trade page

--- src/views/Portal/Trade/components/OrderPreviewModal.js
+++ src/views/Portal/Trade/components/OrderPreviewModal.js
@@ -1,5 +1,5 @@
 // OrderPreviewModal.js
-import React from "react";
+import React, { useState } from "react";
 import {
   Modal,
   ModalOverlay,
@@ -12,6 +12,28 @@ import {
 } from "@chakra-ui/react";
 
 const OrderPreviewModal = ({ isOpen, onClose, formData, teamData }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const submitOrder = async () => {
+    console.log(`submitting order: ${formData}`)
+    setIsLoading(true);
+    try {
+      const response = await fetch("https://l2g6kvzxpa.execute-api.us-east-1.amazonaws.com/dev/api?league=MLB&type=BUY", {
+        method: "POST",
+        body: JSON.stringify({
+          orderData: formData
+        })
+      });
+      const data = await response.json();
+      console.log(data)
+      onClose();
+    } catch (error) {
+      console.error("Error submitting order:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Modal
       isOpen={isOpen}
@@ -49,7 +71,7 @@ const OrderPreviewModal = ({ isOpen, onClose, formData, teamData }) => {
           <Button colorScheme="green" mr={3} onClick={onClose} variant={"outline"}>
             Cancel
           </Button>
-          <Button colorScheme="green">Submit</Button>
+          <Button colorScheme="green" onClick={submitOrder}>Submit</Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
